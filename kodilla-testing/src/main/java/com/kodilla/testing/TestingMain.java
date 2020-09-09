cbafcc2d8d86ec05b7195a1f29b455ebde528703
KChwistek <66901714+KChwistek-prog@users.noreply.github.com>
2020-09-09 20:00:15 +0200

    6.2 Added calculator testing

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,38 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleuser = new SimpleUser("theForumUser");
+
+        String result = simpleuser.getUserName();
+
+        if(result.equals("theForumUser")){
+            System.out.println("test Ok");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator(5,8);
+        Integer resultAdd = calculator.add();
+        Integer resultSub = calculator.subtract();
+        int calcA = calculator.getA();
+        int calcB = calculator.getB();
+
+        if(resultAdd.equals(calcA + calcB)){
+            System.out.println("add test Ok");
+        } else {
+            System.out.println("Error");
+        }
+
+        if(resultSub.equals(calcA - calcB)){
+            System.out.println("subtract test Ok");
+        } else {
+            System.out.println("Error");
+        }
+
+
     }
 }
