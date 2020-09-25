2f9e489980ef1299d171e3379aa2f48e7ea14697
KChwistek <66901714+KChwistek-prog@users.noreply.github.com>
2020-09-25 14:53:29 +0200

    6.6 Mockito + JUnit booklibrary cleanup

--- kodilla-testing/src/main/java/com/kodilla/testing/library/BookLibrary.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/library/BookLibrary.java
@@ -16,17 +16,11 @@ public class BookLibrary {
                 .listBooksWithCondition(titleFragment);
         if (titleFragment.length() < 3) return bookList;
         bookList = resultList;
-        if (resultList.size() > 20) return bookList;
 
         return bookList;
     }
 
     public List<Book>listBooksInHandsOf(LibraryUser libraryUser){
-        List<Book> resultList = libraryDatabase.listBooksInHandsOf(libraryUser);
-        List<Book> bookList = new ArrayList<>();
-        if(resultList.size() == 0) {
-            return bookList;
-        }
-        return resultList;
+        return libraryDatabase.listBooksInHandsOf(libraryUser);
     }
 }
