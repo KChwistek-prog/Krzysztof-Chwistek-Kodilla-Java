8fbd68a5939181a4182488c45f29d4e80c0bd4d4
KChwistek <66901714+KChwistek-prog@users.noreply.github.com>
2020-09-29 18:55:27 +0200

    8.2 try catch

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -3,29 +3,20 @@ package com.kodilla.exception.test;
 public class FirstChallenge {
 
     public double divide(double a, double b) throws ArithmeticException {
-        if (b == 0){
+        if (b == 0) {
             throw new ArithmeticException();
         }
         return a / b;
     }
 
-    /**
-     * This main can throw an ArithmeticException!!!
-     * @param args
-     */
     public static void main(String[] args) {
 
         FirstChallenge firstChallenge = new FirstChallenge();
-
-        //double result = firstChallenge.divide(3, 0);
-
-        //System.out.println(result);
-
-
-        try { double result = firstChallenge.divide(6, 0);
+        try {
+            double result = firstChallenge.divide(6, 0);
             System.out.println(result);
         } catch (ArithmeticException e) {
-            System.out.println("Pamietaj cholero nie dziel przez \n B ma być różne od 0");
+            System.out.println("Pamietaj cholero nie dziel przez 0 \nB ma być różne od 0");
         }
 
     }
