409f52dda5478ffb82514270ac94abd73fa1b893
KChwistek <66901714+KChwistek-prog@users.noreply.github.com>
2020-10-01 13:05:42 +0200

    FileReader examples

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReaderWithoutHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReaderWithoutHandling.java
@@ -8,11 +8,11 @@ import java.util.stream.Stream;
 
 public class FileReaderWithoutHandling {
 
-    public void readFile() throws FileReaderException {
+    public void readFile() throws IOException {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("names.txt").getFile());
-        //Stream<String> fileLines = Files.lines(Paths.get(file.getPath()));
-        //fileLines.forEach(System.out::println);
+        Stream<String> fileLines = Files.lines(Paths.get(file.getPath()));
+        fileLines.forEach(System.out::println);
     }
 
 }
--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -7,10 +7,6 @@ public class ExceptionModuleRunner {
 
     public static void main(String[] args) {
         FileReader fileReader = new FileReader();
-        try {
-            fileReader.readFile();
-        } catch (FileReaderException e) {
-            System.out.println("Problem while reading a file!");
-        }
+        fileReader.readFile();
     }
 }
