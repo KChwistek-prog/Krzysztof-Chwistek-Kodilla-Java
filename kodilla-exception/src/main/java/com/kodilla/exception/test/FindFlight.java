fd6a2186c55a9424c0476776210c9091b8e6483f
KChwistek <66901714+KChwistek-prog@users.noreply.github.com>
2020-10-07 02:07:28 +0200

    7.4 flatmap + reduce

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FindFlight.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FindFlight.java
@@ -3,14 +3,29 @@ package com.kodilla.exception.test;
 import java.util.*;
 
 public class FindFlight {
-    public void findFilght(Flight flight) {
+
+    public static void main(String[] args) throws RouteNotFoundException {
+        FindFlight findFlight = new FindFlight();
+        Flight krakowToSosnowiec = new Flight("Krakow", "Sosnowiec");
+        findFlight.findFlight(krakowToSosnowiec);
+    }
+
+    public void findFlight(Flight flight) throws RouteNotFoundException {
+
+        String start = flight.getDepartureAirport();
+        String end = flight.getArrivalAirport();
 
         Map<String, Boolean> flightMap = new HashMap<>();
-        flightMap.put("Berlin", true);
+        flightMap.put("Berlin", false);
         flightMap.put("Warsaw", true);
-        flightMap.put("Krakow", true);
-        flightMap.put("Barcelona", false);
-        flightMap.put("Hamburg", true);
-        flightMap.put("Sosnowiec", false);
+        flightMap.put("Wąchock", true);
+        flightMap.put("Krakow", false);
+        flightMap.put("Barcelona", true);
+        flightMap.put("Sosnowiec", true);
+
+        if (!flightMap.get(start) || !flightMap.get(end)) {
+            throw new RouteNotFoundException();
+        }
+
     }
 }
--- kodilla-exception/src/main/java/com/kodilla/exception/test/RouteNotFoundException.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/RouteNotFoundException.java
@@ -1,5 +1,15 @@
 package com.kodilla.exception.test;
 
 public class RouteNotFoundException extends Throwable {
+    public static void main(String[] args) {
 
+        try {
+            FindFlight findFlight = new FindFlight();
+            Flight krakowToSosnowiec = new Flight("Krakow", "Sosnowiec");
+            findFlight.findFlight(krakowToSosnowiec);
+
+        } catch (RouteNotFoundException e) {
+            System.out.println("No such flight");
+        }
+    }
 }
