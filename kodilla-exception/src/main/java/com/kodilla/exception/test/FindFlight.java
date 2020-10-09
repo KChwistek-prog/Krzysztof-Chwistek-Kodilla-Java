2d22ec5607cf51b169185587521d17ac78796487
KChwistek <66901714+KChwistek-prog@users.noreply.github.com>
2020-10-09 18:18:07 +0200

    8.4

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FindFlight.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FindFlight.java
@@ -5,42 +5,45 @@ import java.util.Map;
 
 public class FindFlight {
 
+    public Map<String,Boolean> ListOfAvailableAirports() {
+        Map<String,Boolean> availableAirports = new HashMap<>();
+        availableAirports.put("Berlin", true);
+        availableAirports.put("Warsaw", true);
+        availableAirports.put("Wachock", true);
+        availableAirports.put("Krakow", false);
+        availableAirports.put("Barcelona", true);
+        return availableAirports;
+    }
+
     public static void main(String[] args) throws RouteNotFoundException {
         FindFlight findFlight = new FindFlight();
         FlightList flightList = new FlightList();
+
         flightList.addFlight(new Flight("Sosnowiec", "Wachock"));
         flightList.addFlight(new Flight("Wachock", "Berlin"));
-        findFlight.findFlight(flightList.getFlightList(0));
-    }
+        flightList.addFlight(new Flight("Wachock", "Krakow"));
 
-    public Map<String, Boolean> listOfAvailableAirports() {
-        Map<String, Boolean> airportList = new HashMap<>();
-        airportList.put("Berlin", false);
-        airportList.put("Warsaw", true);
-        airportList.put("Wachock", true);
-        airportList.put("Krakow", false);
-        airportList.put("Barcelona", true);
-        airportList.put("Sosnowiec", false);
-        return airportList;
+        try {
+            findFlight.findFlight(flightList.getFlightList(0));
+        } catch (RouteNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public void findFlight(Flight flight) throws RouteNotFoundException {
-        Map<String, Boolean> airportList = listOfAvailableAirports();
-
-
+        Map<String, Boolean> airportList = ListOfAvailableAirports();
         String end = flight.getArrivalAirport();
         String start = flight.getDepartureAirport();
 
-        if (airportList.containsKey(start) && airportList.containsKey(end)) {
+        if (airportList.containsKey(start) != true || airportList.containsKey(end) != true) {
+            throw new RouteNotFoundException();
+        } else {
             if (airportList.get(start) == true && airportList.get(end) == true) {
                 System.out.println("Flight available");
             } else {
                 System.out.println("Flight unavailable");
             }
-        } else {
-            throw new RouteNotFoundException();
         }
-
 
     }
 }
--- kodilla-exception/src/main/java/com/kodilla/exception/test/RouteNotFoundException.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/RouteNotFoundException.java
@@ -1,18 +1,10 @@
 package com.kodilla.exception.test;
 
-import java.util.ArrayList;
-import java.util.List;
+public class RouteNotFoundException extends Exception {
 
-public class RouteNotFoundException extends Throwable {
-    public static void main(String[] args) {
-        try {
-            FindFlight findFlight = new FindFlight();
-            FlightList flightList = new FlightList();
-            flightList.addFlight(new Flight("xx", "Wachock"));
-            flightList.addFlight(new Flight("Wachock", "Berlin"));
-            findFlight.findFlight(flightList.getFlightList(0));
-        } catch (RouteNotFoundException e) {
-            System.out.println("Airport doesnt exist in list");
-        }
+    private static final String EXCEPTION_MESSAGE = "Error! Airport dont exist!";
+
+    public RouteNotFoundException() {
+        super(EXCEPTION_MESSAGE);
     }
 }
