4d81f29f6b84b2327f347a70293bdb44767597e4
KChwistek <66901714+KChwistek-prog@users.noreply.github.com>
2021-02-20 13:16:17 +0100

    24.3 zadanie

--- kodilla-paterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacade.java
+++ kodilla-paterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacade.java
@@ -1,14 +1,18 @@
 package com.kodilla.patterns2.facade.api;
 
 import com.kodilla.patterns2.facade.ShopService;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 
-
+@Component
+@EnableAspectJAutoProxy
 @Service
 public class OrderFacade {
     private static final Logger LOGGER = LoggerFactory.getLogger(OrderFacade.class);
