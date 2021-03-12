eb853ba44032e74ebdfc8b81c844cfb3ec1d753a
KChwistek <66901714+KChwistek-prog@users.noreply.github.com>
2021-03-12 15:39:15 +0100

    31.2 zadanie dziala

--- kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -8,16 +8,24 @@ import org.openqa.selenium.support.ui.Select;
 
 public class FacebookTestingApp {
     public static final String XPATH_ACCEPT = "//div[contains(@class, \"uiLayer\")]/div/div/div/div/div/div/button[2]";
+    public static final String XPATH_REG = "//div[contains(@id, \"reg_pages_msg\")]/a";
+    public static final String XPATH_SIGNUP = "//span[contains(@class, \"signup_box_content\")]/a";
     public static final String XPATH_DAY = "//div[contains(@id, \"birthday_wrapper\")]/div[2]/span/span/select[contains (@id, \"day\")]";
     public static final String XPATH_MONTH = "//div[contains(@id, \"birthday_wrapper\")]/div[2]/span/span/select[2]";
     public static final String XPATH_YEAR = "//div[contains(@id, \"birthday_wrapper\")]/div[2]/span/span/select[3]";
 
     public static void main(String[] args) {
         WebDriver webDriver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
-        webDriver.get("https://www.facebook.com/r.php?locale=pl_PL");
+        webDriver.get("https://www.facebook.com");
 
         WebElement acceptPolicy = webDriver.findElement(By.xpath(XPATH_ACCEPT));
         acceptPolicy.click();
+
+        WebElement msg = webDriver.findElement(By.xpath(XPATH_REG));
+        msg.click();
+
+        WebElement signup = webDriver.findElement(By.xpath(XPATH_SIGNUP));
+        signup.click();
 
         WebElement dayCombo = webDriver.findElement(By.xpath(XPATH_DAY));
         Select selectDay = new Select(dayCombo);
